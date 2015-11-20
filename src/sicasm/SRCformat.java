bba2979f539c84bd450f60e7c26adb2f6e34a444
basmaadrii <basma.adrii@gmail.com>
2015-11-20 16:12:36 +0200

    Create SRCformat Parser

---
+++ src/sicasm/SRCformat.java
@@ -0,0 +1,76 @@
+package sicasm;
+
+import static jdk.nashorn.internal.objects.NativeString.trim;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author TOSHIBA
+ */
+public class SRCformat {
+    private final String Instruction;
+    private String Label;
+    private String OPCode;
+    private String Operand;
+    private boolean errorFlag;
+    
+    SRCformat (String Instruction){
+        this.Instruction = Instruction;
+        errorFlag = false;
+    }
+    private boolean checkFormat(){
+        return !(Instruction.charAt(8) != ' ' || Instruction.charAt(15) != ' ' || Instruction.charAt(16) != ' ');
+    }
+    public void splitInstruction(){
+        String label = Instruction.substring(0, 8);
+        String opcode = Instruction.substring(9, 15);
+        String operand;
+        if (Instruction.length() <= 35)
+            operand = trim(Instruction.substring(17, Instruction.length()));
+        else
+            operand = trim(Instruction.substring(17, 35));
+        
+        if (label.startsWith(" ") || opcode.startsWith(" ") || operand.startsWith(" "))
+            errorFlag = true;
+        else{
+            Label = trim(label);
+            OPCode = trim (opcode);
+            Operand = trim (operand);
+        }
+    }
+
+    /**
+     * @return the Label
+     */
+    public String getLabel() {
+        return Label;
+    }
+
+    /**
+     * @return the OPCode
+     */
+    public String getOPCode() {
+        return OPCode;
+    }
+
+    /**
+     * @return the Operand
+     */
+    public String getOperand() {
+        return Operand;
+    }
+
+    /**
+     * @return the errorFlag
+     */
+    public boolean isErrorFlag() {
+        return errorFlag;
+    }
+    
+    
+}
